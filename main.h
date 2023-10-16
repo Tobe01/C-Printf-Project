aa6663353edf6cf5cb3dc4f135c5c27e7d7159de
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-15 18:33:33 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef PRINT-F
+#define PRINT-F_
+#define BUF_CAPACITY 1024
+#include <stdarg.h>
+int _straff(const char *);
+int _printf(const char *format, ...);
+int turnover(char *, int *, int *);
+int build_str(char *, char *, int *, int *);
+int build_int(int, char *, int *, int *);
+char *transform_int(int);
+int zero_indicators(char, char *, int *, int *);
+int zero_standard_indicators(va_list, char, char *, int *, int *);
+int watch_data(va_list, char *, int, int);
+#endif //PRINT-F
