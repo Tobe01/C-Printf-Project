dc9f760806ab3907c166da81be28f62f38e3fd36
Tobechi Duru <tobechi.duru001@gmail.com>
2023-10-17 18:50:15 -0700

    fix

--- printf.c
+++ printf.c
@@ -7,38 +7,35 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-
-	va_start(args, format);
-
-
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-				count += print_char(va_arg(args, int));
-			else if (*format == 's')
-				count += print_str(va_arg(args, char*));
-			else if (*format == 'd' || *format == 'i')
-				count += print_int(va_arg(args, int));
-			else if (*format == 'u')
-				count += print_uint(va_arg(args, unsigned int));
-			else if (*format == 'x')
-				count += print_hex(va_arg(args, unsigned int), 0);
-			else if (*format == 'X')
-				count += print_hex(va_arg(args, unsigned int), 1);
-			else if (*format == 'p')
-				count += print_pointer(va_arg(args, void*));
-			else if (*format == '%')
-				count += print_char('%');
-		}
-		else
-			count += write(1, format, 1);
-		format++;
-	}
-	va_end(args);
-	return (count);
+int count = 0;
+va_list args;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+if (*format == 'c')
+count += print_char(va_arg(args, int));
+else if (*format == 's')
+count += print_str(va_arg(args, char*));
+else if (*format == 'd' || *format == 'i')
+count += print_int(va_arg(args, int));
+else if (*format == 'u')
+count += print_uint(va_arg(args, unsigned int));
+else if (*format == 'x')
+count += print_hex(va_arg(args, unsigned int), 0);
+else if (*format == 'X')
+count += print_hex(va_arg(args, unsigned int), 1);
+else if (*format == 'p')
+count += print_pointer(va_arg(args, void*));
+else if (*format == '%')
+count += print_char('%');
+}
+else
+count += write(1, format, 1);
+format++;
+}
+va_end(args);
+return (count);
 }
